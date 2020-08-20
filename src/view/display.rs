d761115c51d8d5630c602cbd49f2267bac6d168d
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-08-20 23:34:56 +0900

    Update Cargo.lock and fix build error

--- src/view/display.rs
+++ src/view/display.rs
@@ -7,8 +7,8 @@ pub fn display(width: u32, height: u32, event_loop: &EventsLoop) -> glium::Displ
         .with_dimensions((width, height).into())
         .with_always_on_top(true)
         .with_title("Poin")
-        //.with_transparency(true)
-        //.with_titlebar_hidden(true)
+        .with_transparency(true)
+        .with_titlebar_hidden(true)
         .with_titlebar_buttons_hidden(true);
     let context = glium::glutin::ContextBuilder::new()
         .with_vsync(true)
