a6f37583227f017003fb01ecb02b113d792fa474
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-08-26 16:10:49 +0900

    Search proto file

--- build.rs
+++ build.rs
@@ -1,12 +1,24 @@
+use std::fs;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().build_server(false).compile(
-        &[
-            "../qoin/qoin/proto/hello.proto",
-            "../qoin/qoin/proto/landmark.proto",
-            "../qoin/qoin/proto/face_mesh.proto",
-            "../qoin/qoin/proto/hand_tracking.proto",
-        ],
-        &["../qoin"],
-    )?;
+    let proto_dir = "../qoin/qoin/proto";
+    let files: Vec<String> = fs::read_dir(proto_dir)?
+        .map(|p| {
+            p.unwrap()
+                .path()
+                .file_name()
+                .unwrap()
+                .to_os_string()
+                .into_string()
+                .unwrap()
+        })
+        .filter(|f| f.ends_with(".proto"))
+        .map(|f| format!("{}/{}", proto_dir, f))
+        .collect();
+    let includes: Vec<String> = vec!["../qoin"].iter().map(|s| s.to_string()).collect();
+    println!("{:?}", files);
+    tonic_build::configure()
+        .build_server(false)
+        .compile(&files, &includes)?;
     Ok(())
 }
